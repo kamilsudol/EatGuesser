dabd2078a49e43ad265f02338ed27df0b0ef2ef8
Michał Kuś <kusmichal06@gmail.com>
2020-04-29 21:42:42 +0200

    Working database

--- database/skrypt.py
+++ database/skrypt.py
@@ -14,19 +14,28 @@ fields = ["name",
 filename = "data"
 dict_main = {}
 dict_temp = {}
+line_read = []
 
 with open(filename, "r") as data:
     index = 0
     id_no = 1
 
     for line in data:
+        index = 0
+        dict_temp = {}
         line_read = list(line.strip().split('",', 9))
 
-        # print(line_read)
 
         recipe_id = 'recipe' + str(id_no)
 
+        # print(line_read)
         while index < len(fields):
+            line_read[index] = line_read[index].replace(f'{fields[index]}', '')
+            line_read[index] = line_read[index].replace('"', '')
+            line_read[index] = line_read[index].replace('{', '')
+            line_read[index] = line_read[index].replace(':', '')
+            line_read[index] = line_read[index].replace('\\n', '\n')
+            line_read[index] = line_read[index].strip()
             dict_temp[fields[index]] = line_read[index]
             index += 1
 
