f2a276c8428ce12ad9bef7fffb3fd85724110642
Michał Kuś <kusmichal06@gmail.com>
2020-04-29 20:18:47 +0200

    Almost working database

--- database/skrypt.py
+++ database/skrypt.py
@@ -1,27 +1,57 @@
 import json
-import numpy as np
+# import numpy as np
 
-file = open("data","r")
+fields = ["name",
+          "ingredients",
+          "url",
+          "image",
+          "cookTime",
+          "recipeYield",
+          "datePublished",
+          "prepTime",
+          "description"]
 
-dict = {
-    "recipes": []
-}
+filename = "data"
+dict_main = {}
+dict_temp = {}
 
-for x in file:
-    #print(str(x))
-    dict["recipes"].append(x)
+with open(filename, "r") as data:
+    index = 0
+    id_no = 1
 
-database = open("initial_database.json","w")
-database.write(json.dumps(dict))
+    for line in data:
+        line_read = list(line.strip().split('",', 9))
 
-"""
-dict = {
-    "recipes":{
-        "name": ...
-        "ingredients": ...
-        etc
-        .
-        .
-    }
-}
-"""
+        # print(line_read)
+
+        recipe_id = 'recipe' + str(id_no)
+
+        while index < len(fields):
+            dict_temp[fields[index]] = line_read[index]
+            index += 1
+
+        dict_main[recipe_id] = dict_temp
+        id_no += 1
+
+database = open("initial_database.json", "w")
+json.dump(dict_main, database, indent = 4)
+database.close()
+
+
+# for x in file:
+#     #print(str(x))
+#     dict["recipes"].append(x)
+
+# database = open("initial_database.json","w")
+# database.write(json.dumps(dict))
+
+
+# dict = {
+#     "recipes":{
+#         "name": ...
+#         "ingredients": ...
+#         etc
+#         .
+#         .
+#     }
+# }
