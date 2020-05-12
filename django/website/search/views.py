3a14b78111c3aa11ac85311b81b50171941fcd1a
Wojciech Kuzmik <wojciech.kuzmik@o2.pl>
2020-05-12 16:51:05 +0200

    fixed reloading page and hyperlinks

--- django/website/search/views.py
+++ django/website/search/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 from .models import Recipes
 from .api_dir.api_app import Api
 
-req = Api().ret_req()
-database2 = req.search_recipe("chicken") # tutaj musimy jakos przekazac stringa uzytkownika
-										 # albo zmienic strukture tego co mamy jakos, i dunno
-
 
 def home(request):
+	req = Api().ret_req()
+	database2 = req.search_recipe("chicken")
 	context = {
 		'database': database2 # Recipes.objects.all()
 	}
