5d4767db54c6f1a27f419cc7a83be8fea4ab391e
kamilsudol <kamilsudolorigin@gmail.com>
2020-05-13 17:24:12 +0200

    added env variable

--- django/website/website/settings.py
+++ django/website/website/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pxq-^!c3j*a^ha+-^6xoggqyzc(aik&5qdefa25+p(lg+3yf&e'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
