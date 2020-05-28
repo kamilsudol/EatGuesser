eec81ed3c5d6c05b83a8dfb2a850e05bfadee71d
JJ807 <jakubstrugaa@gmail.com>
2020-05-27 17:52:10 -0700

    cleaning up

--- django/website/users/models.py
+++ django/website/users/models.py
@@ -1,18 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-<<<<<<< HEAD
 # Create your models here.
 class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pictures')
-=======
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
->>>>>>> 7a3f3b372e6c30bfb5be3feb2c735d3de519caf8
 
     def __str__(self):
         return f'{self.user.username} Profile'
--- django/website/users/signals.py
+++ django/website/users/signals.py
@@ -12,8 +12,4 @@ def create_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-<<<<<<< HEAD
     instance.profile.save()
-=======
-    instance.profile.save()
->>>>>>> 7a3f3b372e6c30bfb5be3feb2c735d3de519caf8
